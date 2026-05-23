488825866bdfe6c02f6ba59633c3821caade793d
GAUM-G <gautamgupta986047@gmail.com>
2026-05-24 02:21:39 +0530

    update

--- parse.java
+++ parse.java
@@ -1,8 +1,8 @@
 public class parse {
     public static void main (String[] args) {
         int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int c = a+b ;
+        float b = Float.parseFloat(args[1]);
+        float c = a+b ;
         System.out.println("Sum = " +c);
         System.out.println("hello");
     }
